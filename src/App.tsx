8f83df0c7dc15d4742dd652a55e2bf3f25ad2b15
Jarek <jarek_m04@wp.pl>
2023-08-09 18:42:50 +0200

    .

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import Home from "@pages/Home";
 import Register from "@pages/Register";
 import SignIn from "@pages/SignIn";
 import Cart from "@pages/Cart";
-import { theme } from "@utils/theme";
+// import { theme } from "@utils/theme";
 
 export const Root = () => {
   const router = createBrowserRouter(
